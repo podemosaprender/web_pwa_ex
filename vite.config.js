14806be9fae776046f8d4f016391337dd83a2533
dev@podemosaprender.org <dev@podemosaprender.org>
2024-05-18 03:37:45 +0000

    OK: base for github, ssl

--- vite.config.js
+++ vite.config.js
@@ -1,36 +1,48 @@
-import { VitePWA } from 'vite-plugin-pwa';
+// https://vitejs.dev/config/
 import { defineConfig } from 'vite'
+import { splitVendorChunkPlugin } from 'vite'
 import react from '@vitejs/plugin-react'
+import basicSsl from '@vitejs/plugin-basic-ssl'
+//SEE: https://vitejs.dev/config/server-options.html#server-https
+
+const BasePath= (process.env.GITHUB_REPOSITORY||'').replace(/^[^\/]*/,'') 
+
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA({
-    registerType: 'prompt',
-    injectRegister: false,
+	base: BasePath,
+	plugins: [
+		react(),  
+		basicSsl(), 
+		splitVendorChunkPlugin(),
+		VitePWA({
+			registerType: 'prompt',
+			injectRegister: false,
 
-    pwaAssets: {
-      disabled: false,
-      config: true,
-    },
+			pwaAssets: {
+				disabled: false,
+				config: true,
+			},
 
-    manifest: {
-      name: 'pwa-tut0',
-      short_name: 'pwa-tut0',
-      description: 'pwa tutorial',
-      theme_color: '#000000',
-    },
+			manifest: {
+				name: 'pwa-tut0',
+				short_name: 'pwa-tut0',
+				description: 'pwa tutorial',
+				theme_color: '#000000',
+			},
 
-    workbox: {
-      globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
-      cleanupOutdatedCaches: true,
-      clientsClaim: true,
-    },
+			workbox: {
+				globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+				cleanupOutdatedCaches: true,
+				clientsClaim: true,
+			},
 
-    devOptions: {
-      enabled: false,
-      navigateFallback: 'index.html',
-      suppressWarnings: true,
-      type: 'module',
-    },
-  })],
+			devOptions: {
+				enabled: false,
+				navigateFallback: 'index.html',
+				suppressWarnings: true,
+				type: 'module',
+			},
+		})],
 })
