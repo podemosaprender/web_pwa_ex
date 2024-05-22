acaabdd182e5d192d078d118033ad51e4c10b8c2
dev@podemosaprender.org <dev@podemosaprender.org>
2024-05-22 22:57:11 +0000

    TRY: esc en android

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -8,11 +8,10 @@ import { languages } from '@codemirror/language-data';
 import { Vim, vim } from "@replit/codemirror-vim"
 Vim.vimKeyFromEventOri= Vim.vimKeyFromEvent
 Vim.vimKeyFromEvent= (e,vim) => {
-	console.log(e);
-	alert('CODE '+e.key.charCodeAt(0))
-	if (e.key=='A') { e= {...e, key: 'Escape', charCode: 0, keyCode: 27} }
+	//DBG: console.log(e); alert('CODE '+e.key.charCodeAt(0))
+	if (e.key=='^') { e= {...e, key: 'Escape', charCode: 0, keyCode: 27} }
 	return Vim.vimKeyFromEventOri(e,vim);
-} //A: use £ as ESC on android GBoard
+} //A: use ^ as ESC on android GBoard
 
 export function Editor({value, onChange}) {
 	const onChangeImpl = useCallback((val, viewUpdate) => {
